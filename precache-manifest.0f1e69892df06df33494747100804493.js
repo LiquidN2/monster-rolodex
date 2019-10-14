385f1cd848641e64cdcdd5f382428091f8a9e706
Hugh Nguyen <hhnguyen255@gmail.com>
2019-10-14 14:07:51 +1100

    Updates

---
+++ precache-manifest.0f1e69892df06df33494747100804493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe197de5bfa729d3e319802f73872cf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1e581c6a53497420d811",
+    "url": "/monster-rolodex/static/css/main.62821982.chunk.css"
+  },
+  {
+    "revision": "f0fd004aafba1ae4d4bd",
+    "url": "/monster-rolodex/static/js/2.702c9599.chunk.js"
+  },
+  {
+    "revision": "1e581c6a53497420d811",
+    "url": "/monster-rolodex/static/js/main.47aa9b88.chunk.js"
+  },
+  {
+    "revision": "e1d991a2b9a3476b0dce",
+    "url": "/monster-rolodex/static/js/runtime-main.df1bd6b7.js"
+  }
+]);
